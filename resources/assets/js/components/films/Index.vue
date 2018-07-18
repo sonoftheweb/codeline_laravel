524be0df90803988f6f1b03fe791b5112ceda80b
Jacob Ekanem <prometheus6001@gmail.com>
2018-07-18 21:51:36 +0100

    Initial commit

---
+++ resources/assets/js/components/films/Index.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-app standalone>
+        <v-navigation-drawer persistent :mini-variant.sync="mini" v-model="drawer" overflow>
+            <v-toolbar flat class="transparent">
+                <v-list class="pa-0">
+                    <v-list-tile avatar tag="div">
+                        <v-list-tile-avatar>
+                            <img src="http://68.media.tumblr.com/avatar_119356c63011_128.png" />
+                        </v-list-tile-avatar>
+                        <v-list-tile-content>
+                            <v-list-tile-title>{{ user_name }}</v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </v-list>
+            </v-toolbar>
+            <v-list class="pt-0" dense>
+                <v-divider></v-divider>
+                <li v-for="item in items">
+                    {{ item }}
+                </li>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar fixed class="blue" dark>
+            <v-toolbar-side-icon @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title>{{ title }}</v-toolbar-title>
+            <v-spacer></v-spacer>
+        </v-toolbar>
+        <main>
+            <v-container fluid>
+                <slot></slot>
+            </v-container>
+        </main>
+    </v-app>
+</template>
+<script>
+    export default {
+        props: ['title', 'user_name', 'items'],
+        data () {
+            return {
+                drawer: true,
+                mini: false,
+            }
+        },
+    }
+</script>
